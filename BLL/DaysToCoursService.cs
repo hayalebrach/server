538219d87199d0fc1fc942a0ea8a6c861002a31d
hayalebrach <109691157+hayalebrach@users.noreply.github.com>
2022-12-31 20:12:33 +0200

    שריון מקום מושלם מושלם+מערכת שעות לקורסים

--- BLL/DaysToCoursService.cs
+++ BLL/DaysToCoursService.cs
@@ -8,6 +8,15 @@ namespace BLL
 {
     public class DayToCoursService
     {
-          
+        DAL.Model.DaysToCoursModel model = new DAL.Model.DaysToCoursModel();
+        public List<DTO.DayToCoursDTO> GetTimeByIdPool(int IdPool)
+        {
+            return convert.DaysToCoursConvert.Convert(model.GetTimeByIdPool(IdPool));
+        }
+        //הוספת זמן
+        public DTO.DayToCoursDTO AddDayToCours(DTO.DayToCoursDTO DayToCours)
+        {
+            return convert.DaysToCoursConvert.Convert(model.AddDaysToCours(convert.DaysToCoursConvert.Convert(DayToCours)));
+        }
     }
 }
