2e8aa6fdd177a8057aa660d6d19c7bc7584c3ba9
lielMymon <110356660+lielMymon@users.noreply.github.com>
2023-01-22 06:37:54 +0200

    liel's

--- Client_2/Controllers/ImagesController.cs
+++ Client_2/Controllers/ImagesController.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using BLL;
-using System;
-using System.Collections.Generic;
+
 using System.IO;
-using System.Linq;
+
 using System.Net;
 using System.Net.Http;
 using System.Web;
